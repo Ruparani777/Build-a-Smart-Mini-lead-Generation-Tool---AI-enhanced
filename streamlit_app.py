4e244b9fc3596de32890961ff513af0e63e34ebc
Ruparani777 <96835039+Ruparani777@users.noreply.github.com>
2025-06-12 07:01:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ authenticator = Authenticate(
     config['cookie']['name'],
     config['cookie']['key'],
     config['cookie']['expiry_days'],
-    config['preauthorized']
+   
 )
 
 # Render the login module
